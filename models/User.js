8e5371b9d9b01c3013cd8124fd8178fcb90dfb06
Sivan Kaplan <sivanagar@gmail.com>
2021-12-04 00:31:46 -0800

    user model

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const {Schema , model} = require ('mongoose');
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+    //Must match a valid email address (look into Mongoose's matching validation) 
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Thoughts'
+    }
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ]
+},
+{
+  toJSON: {
+    virtual: true
+  },
+  id: false
+}
+);
+
+const User = model('User', UserSchema);
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+})
+
+module.exports = User;
